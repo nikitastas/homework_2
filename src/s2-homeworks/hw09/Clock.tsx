1748bebc10f4d765807036064f5de4c3b56a38c8
nikitastas <nikitastas1694@gmail.com>
2025-01-13 21:37:08 +0300

    Completed s4 HW9.

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import {restoreState} from '../hw06/localStorage/localStorage'
+import {restoreState, saveState} from '../hw06/localStorage/localStorage'
 import s from './Clock.module.css'
 
 function Clock() {
@@ -44,7 +44,7 @@ function Clock() {
     const stringDate = date.toLocaleString('ru-RU', {
         day: '2-digit',
         month: '2-digit',
-        year: '2-digit',
+        year: 'numeric',
     }) // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
